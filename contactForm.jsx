28ca374e64682536c8c74499c4ec19428e80c33f
kamalendu Das <kamalendudas901@gmail.com>
2026-08-19 23:33:45 +0530

    create contactForm

---
+++ contactForm.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+export const ContactForm = () => {
+  const [contacts, setContacts] = useState({
+    username: "",
+    email: "",
+    message: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setContacts((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(contacts);
+  };
+
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>Contact Form</h1>
+        <form onSubmit={handleFormSubmit}>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            name="username"
+            required
+            autoComplete="off"
+            value={contacts.username}
+            onChange={handleInputChange}
+          />
+
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            required
+            autoComplete="off"
+            value={contacts.email}
+            onChange={handleInputChange}
+          />
+
+          <label htmlFor="message">Message</label>
+          <textarea
+            type="password"
+            name="message"
+            required
+            autoComplete="off"
+            rows="6"
+            value={contacts.message}
+            onChange={handleInputChange}
+          />
+
+          <button type="submit">Send Message</button>
+        </form>
+      </div>
+    </div>
+  );
+};
